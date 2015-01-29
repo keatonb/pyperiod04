3285e30facfb1c214bfcf9f0230bb9d3d2fdae25
Keaton Bell <keatonb@astro.as.utexas.edu>
2015-01-28 22:54:46 -0600

    create initial python script skeleton

---
+++ pyperiod04.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+import os
+
+def ft(lc,lcfile='temp.lc',ftfile='temp.ft',batchfile='temp.bat'):
+    #Write out the light curve file
+    
+    
+    #Write the batch file
+    
+    
+    #Run the batch file
+    
+    
+    #Clean up any temporary files
+        
+    
+    #Read in the FT
+    
+    
+    #Return the FT
+
